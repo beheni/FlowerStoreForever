c0879fa9e7304f86fd38352f4298f2d6503a88a4
beheni <anastasiia.beheni@ucu.edu.ua>
2022-12-08 00:05:12 +0200

    fixes

--- src/main/java/apps/ucu/edu/ua/flower/store/Item.java
+++ src/main/java/apps/ucu/edu/ua/flower/store/Item.java
@@ -1,12 +1,18 @@
 package apps.ucu.edu.ua.flower.store;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public abstract class Item {
-    private String descripton;
-    public String getDescripton(){return descripton;}
-    public abstract double price();
+    private String description;
+    private double price;
+    public String getDescription() {
+        return description;
+    }
 
+    public double getPrice() {
+        return this.price;
+    }
 }
