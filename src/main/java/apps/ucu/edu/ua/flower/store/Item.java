4f661e9aa4e56480b073d664c91dcec3d6860cc4
beheni <anastasiia.beheni@ucu.edu.ua>
2022-12-07 23:52:37 +0200

    added decorators

--- src/main/java/apps/ucu/edu/ua/flower/store/Item.java
+++ src/main/java/apps/ucu/edu/ua/flower/store/Item.java
@@ -1,5 +1,9 @@
 package apps.ucu.edu.ua.flower.store;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public abstract class Item {
     private String descripton;
     public String getDescripton(){return descripton;}
